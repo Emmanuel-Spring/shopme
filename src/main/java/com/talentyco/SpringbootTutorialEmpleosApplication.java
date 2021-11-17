8bdc6cd4d24292878be1945a16e0f74d111387e8
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-11-17 15:04:17 -0300

    Project Config

---
+++ src/main/java/com/talentyco/SpringbootTutorialEmpleosApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialEmpleosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTutorialEmpleosApplication.class, args);
+	}
+
+}
